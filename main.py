d4403953eee5333447a46b6127e42f2183c7b3e5
Alisha-21-cloud <ahmadalishasyed@gmail.com>
2025-07-19 07:56:45 +0530

    Initialize NLP Model and Input Pipeline for JARVIS Query Handling and Execution

--- main.py
+++ main.py
@@ -10,6 +10,24 @@ import os
 import re
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from comtypes import CLSCTX_ALL
+import json
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import random
+import numpy as np
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+model = load_model("chat_model.h5")
+
+with open("tokenizer.pkl", "rb") as f:
+    tokenizer=pickle.load(f)
+
+with open("label_encoder.pkl", "rb") as encoder_file:
+    label_encoder=pickle.load(encoder_file)
+
 
 def initialize_engine():
     engine = pyttsx3.init("sapi5")
@@ -254,6 +272,14 @@ if __name__ == "__main__":
             openApp(query)
         elif any(f"close {app}" in query for app in apps):
             closeApp(query)
+        elif ("what" in query) or ("who" in query) or ("how" in query) or ("hi" in query) or ("thanks" in query) or ("hello" in query):
+                padded_sequences = pad_sequences(tokenizer.texts_to_sequences([query]), maxlen=20, truncating='post')
+                result = model.predict(padded_sequences)
+                tag = label_encoder.inverse_transform([np.argmax(result)])
+
+                for i in data['intents']:
+                    if i['tag'] == tag:
+                        speak(np.random.choice(i['responses']))
         elif "exit" in query:
             sys.exit()
 
